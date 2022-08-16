542b8c20c06f08694b3baf81ff2fe0d4ead1728e
marouf <maroufibrahimi7264@gmail.com>
2022-08-15 19:01:30 -0700

    add active function for header link

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ if (!navigator.onLine) {
    alert('Please, check your connection!');
 }
 
+// active header
+const link = document.querySelectorAll('.link');
+function active_header_link() {
+   link.forEach((item) => item.classList.remove('active'));
+   this.classList.add('active');
+}
+link.forEach((item) => item.addEventListener('click', active_header_link));
+
 // Sidenav
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
